bc05f7a8debc32dc9d35d02a5455ec4f24fb1747
Renan Leite <39325913+renanleite@users.noreply.github.com>
2023-08-23 15:06:05 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -126,8 +126,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = '/home/ubuntu/static'
 STATIC_URL = '/static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
